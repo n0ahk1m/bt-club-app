8e124b60232cb90a37f8da62407a818c03fa533c
3dgyGamer <jakepark2908@gmail.com>
2024-03-10 00:46:45 -0500

    switched to sqlite3 module for ease. will update other branches to fit

---
+++ init/db_init.py
@@ -0,0 +1,37 @@
+import sqlite3
+
+def create_tables():
+    db_master = sqlite3.connect('db/database.db')
+    db = db_master.cursor()
+    #just to make sure initalization does not get appended rather than a new thing being created
+    db.execute("DROP TABLE IF EXISTS users")
+    db.execute("DROP TABLE IF EXISTS clubs")
+    db_master.commit()
+    #the queries to create the user and clubs table
+    creation_queries = [
+        """CREATE TABLE IF NOT EXISTS users (
+           id INTEGER PRIMARY KEY,
+           first_name VARCHAR(255) NOT NULL,
+           last_name VARCHAR(255) NOT NULL,
+           email VARCHAR UNIQUE,
+           email_verification VARCHAR(255),
+           password_hash VARCHAR(255),
+           is_verified BOOLEAN DEFAULT FALSE
+           is_admin BOOLEAN DEFAULT FALSE
+           
+           ) """,
+        """CREATE TABLE IF NOT EXISTS CLUBS (
+            id INTEGER PRIMARY KEY,
+            faculty_name VARCHAR(50),
+            club_name VARCHAR(50) UNIQUE,
+            club_description TEXT,
+            meeting_location VARCHAR(50),
+            meeting_days VARCHAR(50),
+        )
+        """
+    ]
+    for create_query in creation_queries:
+        db.execute(create_query)
+    db_master.commit()
+    db.close()
+    db_master.close()
